952fb64fdeecf82f1d905462b40555ee96725851
Aimi-no <naoko.aimi@gmail.com>
2018-07-24 14:33:36 +0200

    Add files via upload

---
+++ Assets/CameraFollowCursor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowCursor : MonoBehaviour {
+   
+    // Use this for initialization
+    void Start () {
+       Camera mycam  = GetComponent<Camera>();
+        float sensitivity = 0.05f;
+        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
+        vp.x *= sensitivity;
+        vp.y *= sensitivity;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
+        Vector3 sp = mycam.ViewportToScreenPoint(vp);
+
+        Vector3 v = mycam.ScreenToWorldPoint(sp);
+        transform.LookAt(v, Vector3.up);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+         Camera mycam = GetComponent<Camera>();
+        float sensitivity = 0.05f;
+        Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
+        vp.x *= sensitivity;
+        vp.y *= sensitivity;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
+        Vector3 sp = mycam.ViewportToScreenPoint(vp);
+
+        Vector3 v = mycam.ScreenToWorldPoint(sp);
+        transform.LookAt(v, Vector3.up);
+    }
+}
