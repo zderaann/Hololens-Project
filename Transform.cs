d5b7f558eab86dd10d712bb932a1faddd26b8a46
Aimi-no <naoko.aimi@gmail.com>
2019-04-09 15:46:54 +0200

    Add files via upload

---
+++ Transform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//class to get the name of the folder from JSON
+[Serializable]
+public class CalculatedTransform
+{
+    public float scale;
+    public List<float> translation;
+    public List<float> rotation1;
+    public List<float> rotation2;
+    public List<float> rotation3;
+    public List<float> cameras;
+    public Vector3 trans;
+    public Matrix4x4 rot;
+
+}
